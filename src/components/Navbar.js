cb5a81feac838e31886b4101808ca333a94e1cde
asyadnazeem <asyadnazeem@gmail.com>
2023-07-12 14:32:45 +0530

    new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useRef} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 
 export default function Navbar() {
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <div>
             <ul className="Navbar">
                 <div>
                     <h1 className="navbar-header">dev.Navbar</h1>
                 </div>
-                <div activeClassName ="active" className="nav-links">
+                <nav ref={navRef} activeClassName="active" className="nav-links">
                     <li className="Navbar-link">Home</li>
                     <li className="Navbar-link">About</li>
                     <li className="Navbar-link">Projects</li>
                     <li className="Navbar-link">Contact</li>
-                </div>
+                    <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                        <FaTimes/>
+                    </button>
+                </nav>
+                <button className="nav-btn" onClick={showNavbar}>
+                    <FaBars/>
+                </button>
             </ul>
         </div>
     )
