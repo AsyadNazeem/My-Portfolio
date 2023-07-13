c04a8cff549a965b3905e488b32770dbc9172d8b
asyadnazeem <asyadnazeem@gmail.com>
2023-07-13 23:06:01 +0530

    first commit 13th july

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
-import React, {useRef} from "react";
-import {FaBars, FaTimes} from "react-icons/fa";
+import React, { useRef, useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
-
     const navRef = useRef();
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
     const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
+        setIsNavbarOpen(!isNavbarOpen);
+    };
+
+    useEffect(() => {
+        if (isNavbarOpen) {
+            document.body.style.overflow = "hidden"; // Prevent scrolling when navbar is open
+        } else {
+            document.body.style.overflow = "auto"; // Restore scrolling when navbar is closed
+        }
+    }, [isNavbarOpen]);
 
     return (
         <div>
@@ -15,19 +23,19 @@ export default function Navbar() {
                 <div>
                     <h1 className="navbar-header">dev.Asyad</h1>
                 </div>
-                <nav ref={navRef} activeClassName="active" className="nav-links">
+                <nav ref={navRef} className={`nav-links ${isNavbarOpen ? "responsive_nav" : ""}`}>
                     <li className="Navbar-link">Home</li>
                     <li className="Navbar-link">About</li>
                     <li className="Navbar-link">Projects</li>
                     <li className="Navbar-link">Contact</li>
                     <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                        <FaTimes/>
+                        <FaTimes />
                     </button>
                 </nav>
                 <button className="nav-btn" onClick={showNavbar}>
-                    <FaBars/>
+                    <FaBars />
                 </button>
             </ul>
         </div>
-    )
+    );
 }
