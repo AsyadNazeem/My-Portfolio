1d6943c078361befba4595f748808e072e2393c1
asyadnazeem <asyadnazeem@gmail.com>
2023-07-13 19:26:56 +0530

    first commit 13th july

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 
 
 function App() {
     return (
-        <div className="App">
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-            </Routes>
+        <div>
+            <BrowserRouter basename="My-Portfolio">
+                <Navbar/>
+                <Routes>
+                    <Route path="/My-Portfolio" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
