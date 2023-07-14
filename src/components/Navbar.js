769b128be3612a6120661928d1144f91ead9193f
asyadnazeem <asyadnazeem@gmail.com>
2023-07-14 15:27:44 +0530

    first commit 13th july

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, {useRef, useEffect, useState} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
+import {Link} from "react-scroll";
 
 export default function Navbar() {
     const navRef = useRef();
@@ -24,16 +25,42 @@ export default function Navbar() {
                     <h1 className="navbar-header">dev.Asyad</h1>
                 </div>
                 <nav ref={navRef} className={`nav-links ${isNavbarOpen ? "responsive_nav" : ""}`}>
-                    <li className="Navbar-link">Home</li>
-                    <li className="Navbar-link">About</li>
-                    <li className="Navbar-link">Projects</li>
-                    <li className="Navbar-link">Contact</li>
+                    <Link
+                        to="home">
+                        <li className="Navbar-link">Home</li>
+                    </Link>
+                    <Link
+                        className="Navbar-link"
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        <li className="Navbar-link">About</li>
+                    </Link>
+                    <Link to="projects"
+                          activeClass="active"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}>
+                        <li className="Navbar-link">Projects</li>
+                    </Link>
+                    <Link to="contacts"
+                          activeClass="active"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}>
+                        <li className="Navbar-link">Contact</li>
+                    </Link>
                     <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                        <FaTimes />
+                        <FaTimes/>
                     </button>
                 </nav>
                 <button className="nav-btn" onClick={showNavbar}>
-                    <FaBars />
+                    <FaBars/>
                 </button>
             </ul>
         </div>
